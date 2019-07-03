6b9da5f2220c7b2030205da790e9e06dbe9ec9bc
Luis Raga <52186235+xSquallx@users.noreply.github.com>
2019-07-03 17:17:05 -0400

    Luis Raga pagina

---
+++ index.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="e">
+<html>
+
+<head>
+    <link rel="stylesheet" href="/estilos/estilos.css">
+    <link rel="stylesheet" href="owl/owl.carousel.min.css">
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css">
+    <meta charset="utf-8">
+	<title>Luis Raga</title>
+	<meta name ="viewport" content="width=device-width,user-scalable=no">
+</head>
+
+<body>
+	<a id="whatsapp" href="https://api.whatsapp.com/send?phone=4125089130"><img src="imagenes/whatsapp.png" alt=""></a>
+    <header>
+        <div class="menu">
+            <div class="contenedor">
+				<p class="logo">Luis Raga</p>
+				<img class="menu-icon" src="imagenes/menu.png" alt="">
+                <nav>
+                    <ul>
+                        <li><a href="#sobre-mi">Sobre mi</a></li>
+                        <li><a href="#servicios">Servicios</a></li>
+                        <li><a href="#mis-trabajos">Productos</a></li>
+                        <li><a href="#rastreo">Rastreo</a></li>
+                        <li><a href="#por-que-trabajar">¿Por qué trabajar conmigo?</a></li>
+                        <li><a href="#blog">Blog</a></li>
+                        <li><a href="#contacto">Contactos</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="contenedor" id="contenedor-titulo-flex">
+            <div class="contenedor-titulo">
+                <h1>Luis Raga</h1>
+                <h2>Todo lo que necesites, lo consigues aquí.</h2>
+                <a href="">Ver mis trabajos</a>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <section id="sobre-mi">
+            <div class="contedor">
+                <h3>Sobre mi</h3>
+                <div class="contenedor-sobremi">
+                    <div class="computadoras">
+                        <img src="imagenes/computadoras.png" alt="">
+                    </div>
+                    <div class="texto">
+                        <p>Mi nombre es Luis Raga soy TSU. en Informática y estudiante de Ing. Telématica</p>
+                        <p>Tengo 26 años de edad, soy egresado de la casa de estudio Jesús Obrero y bachiller del Colegio La Salle.</p>
+                        <p>Puedo ofrecerte cualquier tipo de computadoras de escritorio y portatil, mantenimiento de computadoras, creación de sistemas y páginas web.
+                        Si no lo tengo, te lo podemos crear.</p>
+                        <a href="">Ver mis trabajos</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="servicios">
+            <div class="contenedor">
+                <h3>Servicios</h3>
+                <div class="contenedor-servicios">
+                    <div class="servicio violeta">
+                        <h4>Diseño Web</h4>
+                        <p>Páginas web atractivas y funcionales con el objetivo de satisfacer todas tus expectativas. La
+                            importancia del diseño es fundamental para tener éxito y atraer a tus visitantes. También lo
+                            es la velocidad de carga del sitio web,
+                            deben ser rápidos para que el visitante tenga la mejor experiencia</p>
+                        <img class="icono" src="imagenes/monitor.png" alt="">
+                        <img class="ondas" src="imagenes/waves.png" alt="">
+                    </div>
+                    <div class="servicio celeste">
+                        <h4>Responsive Design</h4>
+                        <p>Hoy en día las páginas web se ven en multitud de dispositivos como tablets, celulares, libros
+                            electrónicos, notebooks, PCs, etc. La tecnología móvil es el presente y el futuro. Para eso
+                            es importante y me aseguro de que tu sitio web sea compatible con todos los tamaños de
+                            pantalla de esta época moderna</p>
+                        <img class="icono" src="imagenes/mobile.png" alt="">
+                        <img class="ondas" src="imagenes/waves.png" alt="">
+                    </div>
+                    <div class="servicio violeta">
+                        <h4>SEO</h4>
+                        <p>Al crear sitios web, tengo muy en cuenta la optimización de motores de búsqueda (SEO).
+                            Llevo a cabo prácticas de SEO estándar en cada página del sitio web, lo que permite que
+                            tu sitio tenga una clasificación más alta en los resultados de Google y asi generar mas
+                            tráfico a la web</p>
+                        <img class="icono" src="imagenes/seo.png" alt="">
+                        <img class="ondas" src="imagenes/waves.png" alt="">
+                    </div>
+                    <div class="servicio celeste">
+                        <h4>Autoadministrable</h4>
+                        <p>PEs importante que puedas administrar tu página web vos mismo, desde cualquier lugar y en
+                            cualquier momento. Desarrollo sistemas de gestión de contenido que te permiten ver los datos
+                            de tu página web y editar el contenido que quieras, lo que te permite mantenerte actualizado
+                        </p>
+                        <img class="icono" src="imagenes/wordpress.png" alt="">
+                        <img class="ondas" src="imagenes/waves.png" alt="">
+                    </div>
+                    <div class="servicio violeta">
+                        <h4>Mantenimiento</h4>
+                        <p>Podés contactarme si necesitas actualizar tu página web, agregar nuevas secciones o cualquier
+                            cambio que necesites. Esto es perfecto para los sitios web de pequeñas empresas, donde no se
+                            necesita un diseñador de páginas web a tiempo completo, sino solo en distintas ocasiones</p>
+                        <img class="icono" src="imagenes/mante.png" alt="">
+                        <img class="ondas" src="imagenes/waves.png" alt="">
+                    </div>
+                    <div class="servicio celeste">
+                        <h4>Marketing Digital</h4>
+                        <p>Utilizo las herramientas mas importantes como Google Adwords y Google Analytics, para que tu
+                            negocio aparezca en los resultados de búsqueda de Google justo cuando alguien te necesita y
+                            analizar como los usuarios interactúan con tu página web</p>
+                        <img class="icono" src="imagenes/marketing.png" alt="">
+                        <img class="ondas" src="imagenes/waves.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="mis-trabajos">
+            <div class="contenedor">
+                <h3>Productos</h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="item">
+                        <img src="imagenes/pc1.jpg" alt="">
+                        <p>HP 290 G1 Pc Core i5-7400 3.0Ghz, 6GB, 1TB, Win 10 Pro, 18.5 Inch Monitor</p>
+                    </div>
+                    <div class="item">
+                        <img src="imagenes/pc2.png" alt="">
+                        <p>PC Gamming </p>
+                    </div>
+                    <div class="item">
+                        <img src="imagenes/laptop1.jpg" alt="">
+                        <p>Laptop HP 240 G6 - 14" - Intel Core i5-7200U - 8GB - 1TB - Windows 10 Home</p>
+                    </div>
+                    <div class="item">
+                        <img src="imagenes/laptop2.jpg" alt="">
+                        <p>ASUS T102HA-C4-WH Transformer Book 10.1" 2 in 1 Touchscreen Laptop, Intel Quad-Core, White, Pen and Keyboard Included</p>
+                    </div>
+                    <div class="item">
+                        <img src="imagenes/cornetas.jpg" alt="">
+                        <p>Cornetas 2.1 Genius</p>
+                    </div>
+                    <div class="item">
+                        <img src="imagenes/teclado.jpg" alt="">
+                        <p>Teclado y ratón Corsair</p>
+                    </div>
+                    <div class="item">
+                            <img src="imagenes/fuente.jpg" alt="">
+                            <p>Fuente de Poder 600W Certificada</p>
+                        </div>
+                    <div class="item">
+                        <img src="imagenes/tarjeta.png" alt="">
+                        <p>EVGA GeForce GTX 780</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="rastreo">
+            <div class="contenedor">
+                <h3>¿Deseas saber donde va tu compra?</h3>
+                <a href="#">Ingresa aquí</a>
+            </div>
+        </section>
+
+        <section id="por-que-trabajar">
+            <div class="contenedor">
+                <h3>¿Por qué trabajar conmigo?</h3>
+                <div class="contenedor-trabajar">
+                    <div class="item">
+                        <ul>
+                            <li><img src="imagenes/velocidad.png" alt=""></li>
+                            <li>Velocidad</li>
+                            <li>Me aseguro de que tu web cargue entre 2 y 4 segundos (depende de la cantidad de contenido del sitio). Si una web tarda más de 5 segundos en cargar, el 70% de los usuarios lo cierra y no vuelve a entrar más. Y me aseguro que la entrega de tu computadora o pieza ed computadora sea entregada lo más rápido posible.</li>
+                        </ul>
+                    </div>
+                    <div class="item">
+                        <ul>
+                            <li><img src="imagenes/mantenimiento.png" alt=""></li>
+                            <li>Mantenimiento</li>
+                            <li>Mantengo una comunicación constante con mi cliente, para enseñarle a modificar contenido de su página web o solventar inconveniente con sus páginas. También realizamos mantenimiento a sistemas, computadoras y laptop.</li>
+                        </ul>
+                    </div>
+                    <div class="item">
+                        <ul>
+                            <li><img src="imagenes/mobile2.png" alt=""></li>
+                            <li>Dispositivos móviles</li>
+                            <li>Cada vez mas personas navegan desde sus celulares en cualquier parte del mundo. Me aseguro de que tu web esté optimizada para cualquier tipo de dispositivo móvil y puedas ingresar sin problemas.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="blog">
+            <div class="contenedor">
+                <h3>Blog</h3>
+                <div class="contenedor-publicaciones">
+                    <div class="publicacion">
+                    <img src="imagenes/blog1.jpg" alt="">
+                    <div class="contenido">
+                        <h4>No alcanza con tener solamente una página web</h4>
+                        <p>¿Quieres aprender aprender a crear páginas web? No dudes en entras en mi blog para aprender sobre las herramientas básicas para crear tu página.</p>
+                        <a href="">Leer mas ➝</a>
+                    </div>
+                    </div>
+                <div class="publicacion">
+                    <img src="imagenes/blog2.jpg" alt="">
+                    <div class="contenido">
+                        <h4>¿Qué es el marketing Digital y como utilizarlo?</h4>
+                        <p>El Marketing Digital es una de las técnicas mas utlizadas por las mejores companías! ¿Quieres saber de que se trata?</p>
+                        <a href="">Leer mas ➝</a>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="que-esperas">
+            <div class="contenedor">
+                <h3>¿Qué esperas para tener tu página web o el mejor equipo para tu uso?</h3>
+                <a href="https://tawk.to/chat/5ac4ef38d7591465c70930c4/default">Contáctame</a>
+            </div>
+        </section>
+
+        <section id="contacto">
+				<?php
+					if(@$_GET['i']=='ok') { // QUIERE DECIR QUE EL FORMULARIO SE ENVIO CORRECTAMENTE ?>
+			  		<h3>La consulta se envio correctamente. Nos contactaremos a la brevedad.</h3>
+			  		<?php
+					} else{
+					?>
+				<div class="contenedor">
+				  <h3>Contacto</h3>
+				  <form action="envio-formulario.php" method="post" enctype="multipart/form-data">
+					<input type="text" placeholder="Nombre" name="nombre" required>
+					<input type="email" placeholder="Email" name="email" required>
+					<textarea placeholder="Mensaje" name="mensaje" required></textarea>
+					<input type="submit" value="ENVIAR MENSAJE">
+				  </form>
+						<?php } ?>
+					<div class="contacto-info">
+					<div class="mail"><img src="imagenes/mail-icon.png" alt="">luisraga92@gmail.com</div>
+					<div class="whatsapp"><img src="imagenes/whatsapp-icon.png" alt="">0412-5089130</div>
+				  </div>
+			  
+			</div>
+		</section>
+	</main>
+	
+	<footer>
+		<div class="contenedor">
+			<div class="redes">
+				<a href="https://www.facebook.com/XxSquallxX"><img src="imagenes/facebook.png" alt="Facebook"></a>
+				<a href="https://twitter.com/LuisF_Raga"><img src="imagenes/twitter.png" alt="Twitter"></a>
+				<a href="https://www.instagram.com/luisf_raga"><img src="imagenes/instagram.png" alt="Instagram"></a>
+				<a href="https://www.youtube.com/user/SquallVzla"><img src="imagenes/youtube.png" alt="Youtube"></a>
+			</div>
+			<div class="parrafo">
+				<p>2019 Creado por Luis Raga. Todos los derechos reservados</p>
+			</div>
+		</div>
+	</footer>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="owl/owl.carousel.min.js"></script>
+    <script>
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
+                    items: 4
+                }
+            }
+        })
+	</script>
+
+	<!--- Colocar el menú fijo -->
+	<script>
+		$(document).ready(function(){
+			$(window).scroll(function(){
+				scroll = $(window).scrollTop();
+				if (scroll > 100){
+					$('.menu').css({"position":"fixed"});
+    				$('.menu').css({"width":"100%"});
+    				$('.menu').css({"top":"0"});
+    				$('.menu').css({"background":"#fff"});
+				    $('.menu a').css({"color":"#000"});
+				    $('.logo').css({"color":"#000"});
+				    $('.menu').css({"box-shadow":"rgba(0, 0, 0, 0.22) 6px 1px 1px"});
+				    $('.menu').css({"z-index":"100"});
+				} else {
+					$('.menu').css({"position":"relative"});
+    				$('.menu').css({"background":"transparent"});
+    				$('.menu').css({"box-shadow":"0 0 0"});
+    				$('.menu a').css({"color":"#fff"});
+    				$('.logo').css({"color":"#fff"});
+				}
+			})
+			//Menú movil
+			$('.menu-icon').click(function(){
+				$('header nav').slideToggle();				
+			})
+		})
+	</script>
+
+    <!--Inicio del script de Tawk.to-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/5ac4ef38d7591465c70930c4/default';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+            })();
+    </script>
+    <!--Fin Tawk.to Script-->
+</body>
+</html>
